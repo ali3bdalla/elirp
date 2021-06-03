a7297c7d546137544b15fcfbb55ea9d64e8a56cd
3li3bdalla <ali.abdalla@anbit.io>
2021-06-03 08:48:30 +0200

    add account snapshots table

---
+++ app/Models/AccountSnapshot.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Data\CanBeEnabled;
+use App\Data\HasCompany;
+use App\Data\HasUserActions;
+use App\Frame\ModelFrame;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class AccountSnapshot extends ModelFrame
+{
+    use HasFactory;
+    use SoftDeletes;
+    use HasCompany;
+    use HasUserActions;
+}
---
+++ database/migrations/2021_06_03_064707_create_account_snapshots_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountSnapshotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('account_snapshots', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('company_id');
+            $table->unsignedBigInteger('account_id');
+            $table->float('debit', 20, 8)->default(0);
+            $table->float('credit', 20, 8)->default(0);
+            $table->foreign('company_id')->references('id')->on('companies');
+            $table->foreign('account_id')->references('id')->on('accounts');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('account_snapshots');
+    }
+}
