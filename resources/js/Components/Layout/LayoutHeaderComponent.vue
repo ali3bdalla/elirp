59ce76bf05ec95afc80c7f3b21dcf47a39d23c9d
3li3bdalla <ali.abdalla@anbit.io>
2021-06-26 12:56:10 +0200

    add dashboard ui

---
+++ resources/js/Components/Layout/LayoutHeaderComponent.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Layout"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/Layout/LayoutNavbarComponent.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "LayoutNavbarComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
