d0cc0b64d5031c4d4a1062435fd56cef81860b98
3li3bdalla <ali.abdalla@anbit.io>
2021-06-29 22:01:13 +0200

    version  1.0

--- resources/js/Pages/Dashboard/Dashboard.vue
+++ resources/js/Pages/Dashboard/Dashboard.vue
@@ -3,7 +3,7 @@
     <template #title>
       Dashboard
     </template>
-    <div class="">
+    <div class="card-body">
       <!-- Content Row -->
       <div class="row">
         <!-- Earnings (Monthly) Card Example -->
@@ -13,14 +13,14 @@
               <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
                   <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                    Earnings (Monthly)
+                    Invoices Amount
                   </div>
                   <div class="h5 mb-0 font-weight-bold text-gray-800">
-                    $40,000
+                    {{  parseFloat(result.totalInvoicesAmount).toFixed(2) }}
                   </div>
                 </div>
                 <div class="col-auto">
-                  <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                  <!-- <i class="fas fa-calendar fa-2x text-gray-300"></i> -->
                 </div>
               </div>
             </div>
@@ -34,14 +34,14 @@
               <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
                   <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                    Total Users
+                    Invoices
                   </div>
                   <div class="h5 mb-0 font-weight-bold text-gray-800">
-                    {{  result.totalUsers }}
+                    {{  result.totalInvoices }}
                   </div>
                 </div>
                 <div class="col-auto">
-                  <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                  <!-- <i class="fas fa-dollar-sign fa-2x text-gray-300"></i> -->
                 </div>
               </div>
             </div>
@@ -55,16 +55,16 @@
               <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
                   <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                    Tasks
+                    Bills
                   </div>
                   <div class="row no-gutters align-items-center">
                     <div class="col-auto">
                       <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
-                        50%
+                        {{  result.totalBills }}
                       </div>
                     </div>
                     <div class="col">
-                      <div class="progress progress-sm mr-2">
+                      <!-- <div class="progress progress-sm mr-2">
                         <div
                           class="progress-bar bg-info"
                           role="progressbar"
@@ -73,12 +73,11 @@
                           aria-valuemin="0"
                           aria-valuemax="100"
                         ></div>
-                      </div>
+                      </div> -->
                     </div>
                   </div>
                 </div>
                 <div class="col-auto">
-                  <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
                 </div>
               </div>
             </div>
@@ -92,12 +91,14 @@
               <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
                   <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                    Pending Requests
+                    User
                   </div>
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                  <div class="h5 mb-0 font-weight-bold text-gray-800">
+                    {{  result.totalUsers }}
+                  </div>
                 </div>
                 <div class="col-auto">
-                  <i class="fas fa-comments fa-2x text-gray-300"></i>
+                  <!-- <i class="fas fa-comments fa-2x text-gray-300"></i> -->
                 </div>
               </div>
             </div>
@@ -105,333 +106,23 @@
         </div>
       </div>
 
-      <!-- Content Row -->
-
-      <div class="row">
-        <!-- Area Chart -->
-        <div class="col-xl-8 col-lg-7">
-          <div class="card shadow mb-4">
-            <!-- Card Header - Dropdown -->
-            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-              <h6 class="m-0 font-weight-bold text-primary">
-                Earnings Overview
-              </h6>
-              <div class="dropdown no-arrow">
-                <a
-                  class="dropdown-toggle"
-                  href="#"
-                  role="button"
-                  id="dropdownMenuLink"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                </a>
-                <div
-                  class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                  aria-labelledby="dropdownMenuLink"
-                >
-                  <div class="dropdown-header">Dropdown Header:</div>
-                  <a
-                    class="dropdown-item"
-                    href="#"
-                  >Action</a>
-                  <a
-                    class="dropdown-item"
-                    href="#"
-                  >Another action</a>
-                  <div class="dropdown-divider"></div>
-                  <a
-                    class="dropdown-item"
-                    href="#"
-                  >Something else here</a>
-                </div>
-              </div>
-            </div>
-            <!-- Card Body -->
-            <div class="card-body">
-              <div class="chart-area">
-                <canvas id="myAreaChart"></canvas>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <!-- Pie Chart -->
-        <div class="col-xl-4 col-lg-5">
-          <div class="card shadow mb-4">
-            <!-- Card Header - Dropdown -->
-            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-              <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
-              <div class="dropdown no-arrow">
-                <a
-                  class="dropdown-toggle"
-                  href="#"
-                  role="button"
-                  id="dropdownMenuLink"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                </a>
-                <div
-                  class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                  aria-labelledby="dropdownMenuLink"
-                >
-                  <div class="dropdown-header">Dropdown Header:</div>
-                  <a
-                    class="dropdown-item"
-                    href="#"
-                  >Action</a>
-                  <a
-                    class="dropdown-item"
-                    href="#"
-                  >Another action</a>
-                  <div class="dropdown-divider"></div>
-                  <a
-                    class="dropdown-item"
-                    href="#"
-                  >Something else here</a>
-                </div>
-              </div>
-            </div>
-            <!-- Card Body -->
-            <div class="card-body">
-              <div class="chart-pie pt-4 pb-2">
-                <canvas id="myPieChart"></canvas>
-              </div>
-              <div class="mt-4 text-center small">
-                <span class="mr-2">
-                  <i class="fas fa-circle text-primary"></i> Direct
-                </span>
-                <span class="mr-2">
-                  <i class="fas fa-circle text-success"></i> Social
-                </span>
-                <span class="mr-2">
-                  <i class="fas fa-circle text-info"></i> Referral
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- Content Row -->
-      <div class="row">
-        <!-- Content Column -->
-        <div class="col-lg-6 mb-4">
-          <!-- Project Card Example -->
-          <div class="card shadow mb-4">
-            <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">Projects</h6>
-            </div>
-            <div class="card-body">
-              <h4 class="small font-weight-bold">
-                Server Migration <span class="float-right">20%</span>
-              </h4>
-              <div class="progress mb-4">
-                <div
-                  class="progress-bar bg-danger"
-                  role="progressbar"
-                  style="width: 20%;"
-                  aria-valuenow="20"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                ></div>
-              </div>
-              <h4 class="small font-weight-bold">
-                Sales Tracking <span class="float-right">40%</span>
-              </h4>
-              <div class="progress mb-4">
-                <div
-                  class="progress-bar bg-warning"
-                  role="progressbar"
-                  style="width: 40%;"
-                  aria-valuenow="40"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                ></div>
-              </div>
-              <h4 class="small font-weight-bold">
-                Customer Database <span class="float-right">60%</span>
-              </h4>
-              <div class="progress mb-4">
-                <div
-                  class="progress-bar"
-                  role="progressbar"
-                  style="width: 60%;"
-                  aria-valuenow="60"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                ></div>
-              </div>
-              <h4 class="small font-weight-bold">
-                Payout Details <span class="float-right">80%</span>
-              </h4>
-              <div class="progress mb-4">
-                <div
-                  class="progress-bar bg-info"
-                  role="progressbar"
-                  style="width: 80%;"
-                  aria-valuenow="80"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                ></div>
-              </div>
-              <h4 class="small font-weight-bold">
-                Account Setup <span class="float-right">Complete!</span>
-              </h4>
-              <div class="progress">
-                <div
-                  class="progress-bar bg-success"
-                  role="progressbar"
-                  style="width: 100%;"
-                  aria-valuenow="100"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                ></div>
-              </div>
-            </div>
-          </div>
-
-          <!-- Color System -->
-          <div class="row">
-            <div class="col-lg-6 mb-4">
-              <div class="card bg-primary text-white shadow">
-                <div class="card-body">
-                  Primary
-                  <div class="text-white-50 small">#4e73df</div>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-6 mb-4">
-              <div class="card bg-success text-white shadow">
-                <div class="card-body">
-                  Success
-                  <div class="text-white-50 small">#1cc88a</div>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-6 mb-4">
-              <div class="card bg-info text-white shadow">
-                <div class="card-body">
-                  Info
-                  <div class="text-white-50 small">#36b9cc</div>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-6 mb-4">
-              <div class="card bg-warning text-white shadow">
-                <div class="card-body">
-                  Warning
-                  <div class="text-white-50 small">#f6c23e</div>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-6 mb-4">
-              <div class="card bg-danger text-white shadow">
-                <div class="card-body">
-                  Danger
-                  <div class="text-white-50 small">#e74a3b</div>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-6 mb-4">
-              <div class="card bg-secondary text-white shadow">
-                <div class="card-body">
-                  Secondary
-                  <div class="text-white-50 small">#858796</div>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-6 mb-4">
-              <div class="card bg-light text-black shadow">
-                <div class="card-body">
-                  Light
-                  <div class="text-black-50 small">#f8f9fc</div>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-6 mb-4">
-              <div class="card bg-dark text-white shadow">
-                <div class="card-body">
-                  Dark
-                  <div class="text-white-50 small">#5a5c69</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-6 mb-4">
-          <!-- Illustrations -->
-          <div class="card shadow mb-4">
-            <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>
-            </div>
-            <div class="card-body">
-              <div class="text-center">
-                <img
-                  class="img-fluid px-3 px-sm-4 mt-3 mb-4"
-                  style="width: 25rem;"
-                  src="img/undraw_posting_photo.svg"
-                  alt="..."
-                />
-              </div>
-              <p>
-                Add some quality, svg illustrations to your project courtesy of
-                <a
-                  target="_blank"
-                  rel="nofollow"
-                  href="https://undraw.co/"
-                >unDraw</a>, a constantly updated collection of beautiful svg images that
-                you can use completely free and without attribution!
-              </p>
-              <a
-                target="_blank"
-                rel="nofollow"
-                href="https://undraw.co/"
-              >Browse Illustrations on unDraw &rarr;</a>
-            </div>
-          </div>
-          <!-- Approach -->
-          <div class="card shadow mb-4">
-            <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">
-                Development Approach
-              </h6>
-            </div>
-            <div class="card-body">
-              <p>
-                SB Admin 2 makes extensive use of Bootstrap 4 utility classes in
-                order to reduce CSS bloat and poor page performance. Custom CSS
-                classes are used to create custom components and custom utility
-                classes.
-              </p>
-              <p class="mb-0">
-                Before working with this theme, you should become familiar with
-                the Bootstrap framework, especially the utility classes.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   </app-layout>
 </template>
 
 <script>
 import AppLayout from "../../Layouts/AppLayout";
-import { useQuery } from "@vue/apollo-composable";
+import { useQuery, useResult } from "@vue/apollo-composable";
 import gql from "graphql-tag";
 import { watch } from "vue";
 export default {
   setup() {
     const { result } = useQuery(gql`
       query {
+        totalInvoicesAmount
         totalUsers
+        totalInvoices
+        totalBills
         getUsers {
           data {
             id
@@ -440,11 +131,9 @@ export default {
         }
       }
     `);
-    // watch(result, (value) => {
-    //   console.log(value.totalUsers);
-    // });
+
     return {
-      result,
+      result: useResult(result, {}, result.data),
     };
   },
   components: {
