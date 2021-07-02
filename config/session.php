a824ae07df022fc384f377aa8ec2d403499a47b4
3li3bdalla <ali.abdalla@anbit.io>
2021-07-02 20:25:45 +0200

    add domains to session

--- config/session.php
+++ config/session.php
@@ -154,7 +154,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', null),
+    'domain' => env('SESSION_DOMAIN', 'elirp.com'),
 
     /*
     |--------------------------------------------------------------------------
