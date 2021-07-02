94ab666701fa6cd47825ded4b01dbac69e9e4a7c
3li3bdalla <ali.abdalla@anbit.io>
2021-07-02 16:42:59 +0200

    fix unique email issue in keycloak

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
                 $table->id();
                 $table->string('name')->nullable();
                 $table->string('keycloak_id')->nullable()->unique();
-                $table->string('email')->unique()->nullable();
+                $table->string('email')->nullable();
                 $table->timestamp('email_verified_at')->nullable();
                 $table->string('password')->nullable();
                 $table->rememberToken();
